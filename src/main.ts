c0eac2567dce428a624a13de5bea63bf9f2c7aab
pviniciusm <pvinics@gmail.com>
2021-11-08 16:37:54 -0300

    heroku deploy guide

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import middlewareRoutes from './middleware/middleware';
+import express from "express";
+import cors from "cors";
+import middlewareRoutes from "./middleware/middleware";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use("/middleware", middlewareRoutes);
-
 
 app.listen(8081, () => console.log("Server iniciou"));
